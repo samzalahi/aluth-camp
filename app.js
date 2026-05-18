12eb1885057c3f6ef1f89d0cf91c378169097623
mz <itsmzdev@gmail.com>
2026-05-18 14:13:35 +0300

    added basic CRUD functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 // Connecting to database
@@ -21,7 +22,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Middelwares
-app.use(express.urlencoded({ extended: true })); // to load form data by req
+app.use(express.urlencoded({ extended: true })); // to load/parse form data by req
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -36,10 +38,10 @@ app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.post("/campgrounds/new-camp", async (req, res) => {
-  const newCamp = new Campground(req.body);
-  await newCamp.save();
-  res.redirect(`/campgrounds/${newCamp._id}`);
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
@@ -51,6 +53,28 @@ app.get("/campgrounds/:id", async (req, res) => {
   // Campground.findById(id).then((campground) => {
   //   res.render("campgrounds/show", { campground });
   // });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  // res.send("It worked!!");
+  // const { id } = req.params;
+  // await Campground.updateOne({ _id: id }, { $set: req.body.campground });
+  // Instead above method, there is better way findByIDAndUpdate()
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, req.body.campground, { runValidators: true, new: true });
+  // Colt spread the data and send a copy of the object like below instead of send the whole object like i did above, both are valid way
+  // const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  await Campground.findByIdAndDelete(req.params.id);
+  res.redirect("/campgrounds");
 });
 
 app.listen(3000, () => {
